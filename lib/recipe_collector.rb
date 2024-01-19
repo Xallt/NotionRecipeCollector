5d70718c83dab6f968f0ecae15fcc59ad9232bca
Dmitry <mitya.shabat@gmail.com>
2024-01-19 21:21:21 +0400

    Use correct path for gem

--- lib/recipe_collector.rb
+++ lib/recipe_collector.rb
@@ -1,6 +1,6 @@
 class RecipeCollector
   def get_recipes(notion_api_key)
-    recipe_list = `NOTION_API_KEY=#{notion_api_key} python print_recipes.py`.chomp
+    recipe_list = `NOTION_API_KEY=#{notion_api_key} python #{File.expand_path('../../print_recipes.py', __FILE__)}`.chomp
     recipe_list
   end
 end
